bc0b150f7f7e5180ef55b5937e5c4a3492130d98
Erik Mattheis <erik.mattheis@snhu.edu>
2021-09-24 23:42:47 -0400

    use built-in panache access to mongo collection

--- src/main/java/edu/snhu/erik/mattheis/thermostat/db/ThermostatRepository.java
+++ src/main/java/edu/snhu/erik/mattheis/thermostat/db/ThermostatRepository.java
@@ -2,28 +2,14 @@ package edu.snhu.erik.mattheis.thermostat.db;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import com.mongodb.client.MongoClient;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 
 @ApplicationScoped
 public class ThermostatRepository implements PanacheMongoRepository<Thermostat> {
-	@Inject
-	MongoClient mongo;
-
-	@Inject
-	@ConfigProperty(name = "quarkus.mongodb.database")
-	String databaseName;
-
 	@PostConstruct
 	void init() {
-		mongo.getDatabase(databaseName)
-			.getCollection(Thermostat.COLLECTION)
-			.createIndexes(Thermostat.INDEXES);
+		mongoCollection().createIndexes(Thermostat.INDEXES);
 	}
 	
 	public Thermostat create(String label, String port) {
