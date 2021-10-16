9642e53c527c66bcce682ea0373a43abaf68df1c
Erik Mattheis <erik.mattheis@snhu.edu>
2021-10-15 23:31:45 -0400

    added documentation in comments

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -1,3 +1,23 @@
+/** @author Erik Mattheis <erik.mattheis@snhu.edu> */
+
+/**
+ * constructor for an object to maintain a delay value
+ * that can get progressively larger with each access
+ * 
+ * after initial creation or reset(), the next call to get()
+ * will return {@link initialDelay}
+ * 
+ * each subsequent call to get() will return a progressively
+ * larger value computed by multiplying the previous value
+ * by the {@link backoffFactor}, until {@link maxDelay} is
+ * reached, at which point all calls to get() will return {@link maxDelay}
+ * until reset() is called
+ * 
+ * @constructor
+ * @param initialDelay - the initial value returned by get()
+ * @param maxDelay - the maximum value returned by get()
+ * @param backoffFactor - the amount to multiple values by netween successive calls to get()
+ */
 function RetryDelay(initialDelay, maxDelay, backoffFactor) {
 
 	var _initialDelay = initialDelay;
@@ -21,29 +41,42 @@ function RetryDelay(initialDelay, maxDelay, backoffFactor) {
 	};
 }
 
-var historyRetryDelay = new RetryDelay(100, 60000, 2);
-var updateRetryDelay = new RetryDelay(100, 60000, 1.5);
+var historyRetryDelay = new RetryDelay(100, 60000, 2);  // instance for history polling retries
+var updateRetryDelay = new RetryDelay(100, 60000, 1.5); // instance for update websocket reconnect
 
+/**
+ * formats a temperature by truncating to the nearest tenth and appending '° C'
+ *
+ * @param temp - the temperature to format
+ * @returns the formatted temperature
+ */
 function formatTemperature(temp) {
   return (Math.floor(temp * 10) / 10).toFixed(1) + '° C';
 }
 
+/**
+ * creates an options array for the desired temperature drop-down
+ *
+ * generates options in 0.5 degree intervals from 4.0 through 33.0
+ *
+ * @param selectedTemp - the value to mark selected when generating options
+ * @returns the generated options array
+ */
 function generateTemperatureOptions(selectedTemp) {
   var options = [];
   for (var temp = 4; temp <= 33; temp += 0.5) {
-    options.push($('<option/>', { value: temp, selected: (temp == selectedTemp), text: temp.toFixed(1) + '°' }));
+    options.push($('<option/>', { value: temp, selected: (temp.toFixed(1) == selectedTemp.toFixed(1)), text: temp.toFixed(1) + '°' }));
   }
   return options;
 }
 
-function changeDesiredTemperature(id, temp) {
-  $.post({
-    url: 'api/thermostats/' + id,
-    contentType: 'application/json',
-    data: JSON.stringify({ desiredTemperature: temp })
-  });
-}
-
+/**
+ * adds the given thermostat to the page by constructing
+ * HTML dynamically and appending it to the thermostats
+ * placeholder in the static HTML
+ *
+ * @param thermostat - the thermostat data retrieved from the API
+ */
 function addThermostatCard(thermostat) {
   $('<div/>', { class: 'col' })
   .append($('<div/>', { id: 'thermostat_' + thermostat.id, class: 'card' })
@@ -66,15 +99,39 @@ function addThermostatCard(thermostat) {
     .appendTo($('#thermostats'));
 }
 
+/**
+ * makes an AJAX request to the API to set the desired temperature
+ * for the specified thermostat
+ *
+ * @param id - the identifier of the thermostat to update
+ * @param temp - the desired temperature to set
+ */
+function changeDesiredTemperature(id, temp) {
+  $.post({
+    url: 'api/thermostats/' + id,
+    contentType: 'application/json',
+    data: JSON.stringify({ desiredTemperature: temp })
+  });
+}
+
+/**
+ * begins periodic polling for temperature history for the specified thermostat
+ * over the previous 6 hours via AJAX requests to the API
+ *
+ * the history data is drawn as a graph within the thermostat
+ * card using Plotify
+ *
+ * @param id - the identifier of the thermostat to get history for
+ */
 function updateTemperatureHistory(id) {
-  var to = new Date();
-  var from = new Date(to.getTime() - (6 * 60 * 60 * 1000));
+  var to = new Date();                                      // right now
+  var from = new Date(to.getTime() - (6 * 60 * 60 * 1000)); // six hours ago
   $.get({
     url: 'api/thermostats/' + id + '/temperature/history?from=' + from.toISOString() + '&to=' + to.toISOString(),
     success: function(history) {
       var data = [
         {
-          x: history.timestamps.map(function(timestamp) { return new Date(timestamp); }),
+          x: history.timestamps.map(function(timestamp) { return new Date(timestamp); }), // convert timestamps to Date instances
           y: history.temperatures,
           type: 'scatter',
           line: {
@@ -112,13 +169,25 @@ function updateTemperatureHistory(id) {
   });
 }
 
+/**
+ * connects to the API via websocket to listen for thermostat updates
+ *
+ * when updates arrive, the HTML is dynamically updated to reflect the
+ * latest state
+ *
+ * @param id - the identifier of the thermostat to get history for
+ */
 function subscribeToUpdates(id) {
   var ws = new WebSocket('ws://' + window.location.host + '/api/thermostats/' + id + '/updates');
   ws.onmessage = function(event) {
     var thermostat = JSON.parse(event.data);
+    // set badge to OFF or HEAT accordingly
     $('#thermostat_' + thermostat.id + ' .card-header .badge').removeClass(thermostat.heaterOn ? 'bg-secondary' : 'bg-danger').addClass(thermostat.heaterOn ? 'bg-danger' : 'bg-secondary').text(thermostat.heaterOn ? 'heat' : 'off');
+    // update temperature
     $('#thermostat_' + thermostat.id + ' .card-title').text(formatTemperature(thermostat.ambientTemperature));
+    // update desired temperature value, disabling/enabling the drop-down as necessary
     $('#desired_temp_' + thermostat.id).val(thermostat.desiredTemperature).prop('disabled', thermostat.remoteUpdateDisabled);
+    // display lock/unlock icon as necessary
     $('#thermostat_' + thermostat.id + ' .input-group i').prop('class', thermostat.remoteUpdateDisabled ? 'bi-lock' : 'bi-unlock');
     updateRetryDelay.reset();
   };
@@ -130,12 +199,13 @@ function subscribeToUpdates(id) {
   };
 }
 
+// on document load, make AJAX request to API for configured thermostats
 $(document).ready(function() {
   $.get('api/thermostats', function(thermostats) {
-    for (var i = 0; i < thermostats.length; ++i) {
-      addThermostatCard(thermostats[i]);
-      updateTemperatureHistory(thermostats[i].id);
-      subscribeToUpdates(thermostats[i].id);
+    for (var i = 0; i < thermostats.length; ++i) { // for each configured thermostat...
+      addThermostatCard(thermostats[i]);           //     display thermostat
+      updateTemperatureHistory(thermostats[i].id); //     begin polling for temperature history
+      subscribeToUpdates(thermostats[i].id);       //     listen for thermostat updates
     }
   });
 });
